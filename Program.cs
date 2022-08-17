972e3fa5b0c9d2822484ff0aaaec6ff5455c4e70
Nithina97 <65803545+Nithina97@users.noreply.github.com>
2022-08-17 16:15:41 +0530

    Day and Month
    to print the full name of the current day and current month

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+ class Program{
+        static void Main(string[] args)
+        {
+            DateTime d=DateTime.Now;
+           
+           
+            Console.WriteLine(d.DayOfWeek);   
+            Console.WriteLine(d.ToString("MMMM"));
+           
+
+            Console.ReadLine();
+        }
+        
+    }
